0fb6a5172fd444169b4ecb3feb2987ffbdf60086
aiclassic625 <aiclassic625@gmail.com>
2026-08-14 17:00:16 +0900

    Update app.py

--- app.py
+++ app.py
@@ -6,31 +6,16 @@ from study_helper import generate_study_material
 
 st.set_page_config(page_title="찰칵 중국어", page_icon="📚")
 st.title("📸 찰칵 중국어")
-st.write("책 페이지를 찍거나 갤러리에서 선택하면 중국어 학습 자료를 만들어드려요!")
+st.write("책 페이지 사진을 업로드하면 중국어 학습 자료를 만들어드려요!")
 
-# --- 세션 초기화 ---
-if "uploaded_file" not in st.session_state:
-    st.session_state.uploaded_file = None
+# --- 파일 업로드만 (카메라 제거) ---
+uploaded_file = st.file_uploader("갤러리에서 사진을 선택하세요", type=["jpg", "jpeg", "png", "JPG", "JPEG", "PNG"])
 
-# --- 탭 구성 ---
-tab1, tab2 = st.tabs(["📁 파일 업로드", "📸 카메라 촬영"])
-
-with tab1:
-    uploaded = st.file_uploader("갤러리에서 사진을 선택하세요", type=["jpg", "jpeg", "png", "JPG", "JPEG", "PNG"])
-    if uploaded is not None:
-        st.session_state.uploaded_file = uploaded
-
-with tab2:
-    uploaded = st.camera_input("📸 책 페이지를 카메라로 찍어주세요")
-    if uploaded is not None:
-        st.session_state.uploaded_file = uploaded
-
-# --- 이미지 처리 ---
-if st.session_state.uploaded_file is not None:
+if uploaded_file is not None:
     with open("temp_image.jpg", "wb") as f:
-        f.write(st.session_state.uploaded_file.getbuffer())
+        f.write(uploaded_file.getbuffer())
     
-    image = Image.open(st.session_state.uploaded_file)
+    image = Image.open(uploaded_file)
     st.image(image, caption="업로드된 이미지", use_container_width=True)
     
     if st.button("🚀 학습 자료 생성하기"):
